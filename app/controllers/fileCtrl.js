6257bff22628816659efab07a3b9423202667022
Ruiming <20142100010@m.scnu.edu.cn>
2016-06-15 19:14:42 +0800

    Fix forward & backward

--- app/controllers/fileCtrl.js
+++ app/controllers/fileCtrl.js
@@ -4,7 +4,8 @@ routeApp.controller('fileCtrl', ['$scope', '$interval', function($scope, $interv
     $scope._index = true;
     $scope.path = null;
     $scope.files = [];
-    $scope.history = ["index"];
+    $scope.history = ["index"];     // 记录访问的页面，默认在index页面
+    $scope.temp = [];               // 保存后退记录
     $scope.col = 'Name';
     $scope.desc = 0;
 
@@ -12,59 +13,83 @@ routeApp.controller('fileCtrl', ['$scope', '$interval', function($scope, $interv
 
     // 跳进相应磁盘
     $scope.forward = function(disk) {
-        $scope._index = false;
         $scope.path = disk + "\\\\";
-        $scope.read_folder(1);
+        $scope.history.push($scope.path);        // 每次跳转前记录要访问的路径
+        $scope._index = false;
+        $scope.read_folder();
     };
 
     // 跳进相应文件夹
     $scope.forward_folder = function(path) {
         $scope.path += path + "\\\\";
-        $scope.read_folder(1);
+        $scope.history.push($scope.path);   // 每次跳转前记录要访问的路径
+        $scope.temp = [];                   // 点击进入文件夹会破坏后退记录
+        $scope.read_folder();
     };
 
     // 跳到主页
     $scope.home = function() {
+        $scope.path = "index";
+        $scope.history.push($scope.path);        // 每次跳转前记录要访问的路径
+        console.log($scope.history);
         $scope._index = true;
         $scope.files = [];
-        $scope.history = ["index"];
         getDisk();
     };
 
     // 后退
-    $scope.backward = function() {
-        if($scope.history == null || $scope.history.length < 2){
+    $scope.Hbackward = function() {
+        if($scope.history == null || $scope.history.length == 1){
             return;
         }
-        $scope.history.pop();
+        $scope.temp.push($scope.path);          // 记录当前的路径
+        $scope.history.pop();                   // 从历史记录中移除目前路径
+        $scope.path = $scope.history[$scope.history.length - 1];          // 取出要后退到的路径
         $scope.files = [];
-        $scope.path = $scope.history[$scope.history.length - 1];
         if($scope.path == "index") {
             $scope.filename = null;
             getDisk();
             $scope._index = true;
         }
         else {
-            $scope.read_folder(-1);
+            $scope.read_folder();
+            $scope._index = false;
+        }
+    };
+
+    // 前进
+    $scope.Hforward = function(){
+        if($scope.temp == null || $scope.temp.length < 1){
+            return;
+        }
+        $scope.path = $scope.temp[$scope.temp.length - 1];  // 获取前进的路径
+        $scope.history.push($scope.path);                   // 每次跳转前记录要访问的路径
+        $scope.temp.pop();                                  // 前进记录出栈
+        if($scope.path == "index") {
+            $scope.filename = null;
+            getDisk();
+            $scope._index = true;
+        }
+        else {
+            $scope.read_folder();
+            $scope._index = false;
         }
     };
 
     // 读取路径下的所有文件
-    $scope.read_folder = function(id){
+    $scope.read_folder = function(){
         fs.readdir($scope.path,function(err, files){
             if (err) {
                 console.error(err);
             }
             else {
-                if (id === 1)   $scope.history.push($scope.path);
                 $scope.filename = files;
                 $scope.files = [];
                 for(var i in $scope.filename){
                     if($scope.filename.hasOwnProperty(i)){         // fixme 改成异步方式
                         try{
-                        $scope.file = fs.statSync($scope.path + "////" + $scope.filename[i]);
+                            $scope.file = fs.statSync($scope.path + "////" + $scope.filename[i]);
                         }catch(err){
-                            console.log(err);
                             continue;
                         }
                         $scope.file.name = $scope.filename[i];
