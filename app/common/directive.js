c8d0322dd22416fedadc460ba5768ea5982083ff
Ruiming <20142100010@m.scnu.edu.cn>
2016-06-28 19:05:08 +0800

    Fixed index while sorting

--- app/common/directive.js
+++ app/common/directive.js
@@ -12,3 +12,13 @@ routeApp.directive('focusMe', function($timeout) {
         }
     };
 });
+routeApp.filter('index', function () {
+    return function (array, index) {
+        if (!index)
+            index = 'index';
+        for (var i = 0; i < array.length; ++i) {
+            array[i][index] = i;
+        }
+        return array;
+    };
+});
